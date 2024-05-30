cd29c4d7cfff90af70bc70131a3385495978f2f8
martinmontesjavier <120597788+martinmontesjavier@users.noreply.github.com>
2024-05-30 16:39:38 +0000

    Proyecto montado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-primary d-flex justify-content-center'>Hola</h1>
+    </>
+  )
+}
+
+export default App
