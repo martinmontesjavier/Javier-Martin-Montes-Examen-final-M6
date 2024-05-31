c53fbf1856427e0e254b357e5b78bc243560af60
martinmontesjavier <120597788+martinmontesjavier@users.noreply.github.com>
2024-05-31 15:04:45 +0000

    Borrar HECHO

--- src/componentes/TablaPendientes.jsx
+++ src/componentes/TablaPendientes.jsx
@@ -1,23 +1,26 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
+import { Button } from "react-bootstrap";
+import { Trash2 } from "lucide-react";
 
 export function TablaPendientes() {
-  const { basePendientes } = useContext(GlobalContext);
+  const { basePendientes, setBasePendientes } = useContext(GlobalContext);
 
-  const controladorBorrar = async () => {
+  const controladorBorrar = async (id) => {
     try {
-      const response = await fetch(`https://javier-martin-montes-api-examen-m6.vercel.app/ticketsPendientes${codigo}`, {
+      const response = await fetch(`https://javier-martin-montes-api-examen-m6.vercel.app/ticketsPendientes/${id}`, {
         method: 'DELETE',
       });
 
       if (response.ok) {
-        console.log("Ticket RESUELTO eliminada exitosamente");
-        await fetchHistorias(); // Actualiza el estado global con las historias más recientes
+        console.log("Ticket PENDIENTE eliminado exitosamente");
+        // Actualizar el estado global eliminando el ticket borrado
+        setBasePendientes(basePendientes.filter(ticket => ticket.id !== id));
       } else {
-        console.error("Error al eliminar la ticket RESUELTO");
+        console.error("Error al eliminar el ticket PENDIENTE");
       }
     } catch (error) {
-      console.error("Error en la solicitud para eliminar ticket RESUELTO:", error);
+      console.error("Error en la solicitud para eliminar el ticket PENDIENTE:", error);
     }
   };
 
@@ -25,6 +28,7 @@ export function TablaPendientes() {
     <table className="table mt-4">
       <thead>
         <tr>
+          <th>ID</th>
           <th>Código</th>
           <th>Fecha</th>
           <th>Aula</th>
@@ -39,7 +43,8 @@ export function TablaPendientes() {
       </thead>
       <tbody>
         {basePendientes.map((ticket) => (
-          <tr key={ticket.codigo}>
+          <tr key={ticket.id}>
+            <td>{ticket.id}</td>
             <td>{ticket.codigo}</td>
             <td>{ticket.fecha}</td>
             <td>{ticket.aula}</td>
@@ -50,7 +55,11 @@ export function TablaPendientes() {
             <td><button className="btn btn-success" title="Resolver ticket">Resolver</button></td>
             <td><button className="btn btn-warning" title="Añadir comentario"><i className="bi bi-pencil" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button></td>
             <td><button className="btn btn-info" title="Ver comentarios"><i className="bi bi-chat-left-text"></i></button></td>
-            <td><button className="btn btn-danger" title="Eliminar ticket"><i className="bi bi-trash3"></i></button></td>
+            <td>
+              <Button onClick={() => controladorBorrar(ticket.id)} className="bg-danger" variant="ghost" radius="lg" size="sm">
+                <Trash2 />
+              </Button>
+            </td>
           </tr>
         ))}
       </tbody>
